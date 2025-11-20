b02e2c7b212ac0dbcafc5cfa6c2504be93cdcde7
Walid Elgammal <welgammal@csumb.edu>
2025-11-20 00:44:20 -0800

    Fixed admin role- fix2

--- middleware/requireAdmin.js
+++ middleware/requireAdmin.js
@@ -2,7 +2,7 @@
 module.exports = function requireAdmin(req, res, next) {
   // must be authenticated and admin
   if (!req.user) return res.status(401).json({ error: 'Unauthorized' });
-  if (!req.user.is_admin && !req.user?.roles?.includes?.('admin')) {
+  if (!req.user.is_admin && !req.user?.roles?.includes?.('Administrator')) {
     return res.status(403).json({ error: 'Forbidden' });
   }
   next();
