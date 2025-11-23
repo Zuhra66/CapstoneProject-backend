bf164878d935ba5b6505c362aedc7a5417365dce
Kelia Smith <kelismith@csumb.edu>
2025-11-23 12:17:13 -0800

    Fix CORS for production on render

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ const blogRoutes = require("./routes/blog");
 const eventsRoutes    = require("./routes/events");
 
 
-const app  = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-const PORT = process.env.PORT || 5001; // default 5001 to match your previous setup
+// const app  = express();
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+// const PORT = process.env.PORT || 5001; // default 5001 to match your previous setup
 
 /* ---------- Security hardening ---------- */
 // Behind Render/other proxies; enables req.secure and honors X-Forwarded-* headers
